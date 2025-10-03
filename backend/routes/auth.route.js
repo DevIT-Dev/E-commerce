bd74f7167d302dd7acf1df58e6ce2d4794c62646
Peter-DevX <peterasiedugyan0@gmail.com>
2025-10-03 21:59:03 +0000

    starter code for signup page (SignUpPage.jsx)

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -14,6 +14,6 @@ router.post("/login", login);
 router.post("/signup", signup);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
-router.get("profile", protectRoute, getProfile);
+router.get("/profile", protectRoute, getProfile);
 
 export default router;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { UserPlus, Mail, Lock, User, ArrowRiht, Loader } from "lucide-react"
+import { motion } from "framer-motion";
+
+
 
 const SignUpPage = () => {
+   const loading = true;
+   const [formData, setFormData] = useState({
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+   })
+
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(formData);
+   }
+
+   
   return (
-    <div>SignUpPage</div>
+    <div></div>
   )
 }
 
